5cd0f064d70d2a07f1c70af9ddde8a70c301f22b
Ludovico Rossi <lurossi@e3r3p8.42roma.it>
2026-01-31 14:45:20 +0100

    digits

--- digits.c
+++ digits.c
@@ -1,23 +1,45 @@
-char	*find_line(char number3, int index, char number2)
+char	*find_line(char number1, int index, char number2)
 {
+	char	key[65];
+
 	if (index == 0)
-		//find single number string
-	if (index == 1)
-		//find from 10 to 19 strings
-	if (index == 2)
-		//find from 20 to 90 strings
+			return(fill(number1, '\0', -1, key));
+	else if (index == 1)
+		return(fill(number2, number1, 0, key));
+	else if (index == 2)
+		return(fill(number1, number2, 0, key));
 	else
-		//find other strings
+	{
+		return(fill(number1, '0', (index / 3), key));
+	}
 }
 
-void	print(char *string)
+char	*fill(char number1, char number2, int index, char *key)
+{
+	int	i;
+
+	i = 2;
+	while (index > 0)
+	{
+		key[i] = '0';
+		index--;
+		i++;
+	}
+	key[0] = number1;
+	key[1] = number2;
+	key[i] = '\0';
+
+	return (extract(key));
+}
+
+void	print(char *word)
 {
 	int	i;
 
 	i = 0;
-	while (string[i])
+	while (word[i])
 	{
-		write(1, string[i], 1);
+		write(1, &word[i], 1);
 		i++;
 	}
 }
@@ -27,7 +49,7 @@ void	doubles(char number2, char number3)
 	if (number2 == '0')
 		if(number3 != '0')
 			print(find_line(number3, 0, 0));
-	if (number2 == '1')
+	else if (number2 == '1')
 		print(find_line(number3, 1, number2));
 	else 
 	{
@@ -40,7 +62,7 @@ void	hundreds(char number1, char number2, char number3, int index)
 {
 	if (number1 == '0')
 		doubles(number2, number3);
-	else
+	else  
 	{
 		print(find_line(number1, 0, 0));
 		print(find_line(number1, index + 1, 0));
